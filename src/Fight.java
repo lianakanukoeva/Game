f9aea9ea69f07e66aafa9a3c2c417963eeb2ae8d
kanukoeva.liana@yandex.ru <kanukoeva.liana@yandex.ru>
2021-07-13 20:09:59 +0300

    fix class Fight

--- src/Fight.java
+++ src/Fight.java
@@ -1,34 +1,49 @@
 public class Fight {
 
-    // реализовать функцию с очередью на удар
-    // в зависимости от очереди реализовать удар
-    public void FightResult(int monsterPower, int monsterLife, int heroPower, int heroLife) {
+    public void FightResult(Character hero, Character monster) {
+        // очередь на удар
         boolean turn = true;
-        if (turn == true) {
 
-        } else if (turn == false) {
-
+        while(true) {
+            if(turn == true) {
+                attack(hero, monster);
+                turn = false;
+                if(attack(hero, monster) == false) {
+                    break;
+                }
+            } else {
+                attack(monster, hero);
+                turn = true;
+                if(attack(monster, hero) == false) {
+                    break;
+                }
+            }
         }
-        // жизненная сила одного(кого именно определит очередь) = его жизненная сила - сила удара соперника
     }
 
-    public void attack(Character name, Character enemie, Character att) {
-        int att_ = att.hit();
+    private boolean attack(Character name, Character enemie) {
+        int att_ = name.hit();
+        boolean result = true;
 
-        if(att_ == 0) {
-            System.out.println(name.name + " промазал!");
-        } else if(att_ != 0) {
+        if (att_ == 0) {
+            System.out.println(name.getName() + " промазал!");
+        } else if (att_ != 0) {
             enemie.setLife(name.life -= att_);
             System.out.println(name.getName() + " нанес удар с силой " + att_);
             System.out.println(enemie.getName() + " потерял " + att_ + " очков из жизни.");
         }
 
-        if(name.getLife() <= 0) {
+        if (name.getLife() <= 0) {
             System.out.println(name.getName() + " проиграл...");
-        } else if(enemie.getLife() <= 0) {
+            result = false;
+        } else if (enemie.getLife() <= 0) {
             System.out.println(enemie.getName() + " проиграл игру, а " + name.getName() + "забриает привелегии.");
             name.setGold(name.getGold() + enemie.getGold());
             name.setExperience(name.getExperience() + enemie.getExperience());
+            result = false;
+        } else {
+            result = true;
         }
+        return result;
     }
 }
--- src/Locreuas.java
+++ src/Locreuas.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+
 public class Locreuas {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите имя вашего Героя");
+
+        // hero
+        String heroName = scan.nextLine();
+        Hero hero = new Hero(heroName, 100, 17, 15, 60, 150);
+
+        // monsters
+        Skelleton skelleton = new Skelleton("Скелет", 60, 12, 13, 60, 100);
+        Goblin goblin = new Goblin("Гоблин", 80, 15, 14, 100, 150);
+
+        Fight fight = new Fight();
+        boolean game = true;
+
+        Character monster;
+        // let's go
+        while (game) {
+
+            // choose monster
+            int random = (int) Math.random();
+            if(random % 2 == 0) {
+                monster = goblin;
+            } else {
+                monster = hero;
+            }
+
+            // choose the way
+            int way = scan.nextInt();
+            switch (way) {
+                case 1:
+                    System.out.println("Тогда пойдемте к торговцу.");
+                case 2:
+                    System.out.println("Сражайтесь, раз уж пошли в темный лес.");
+                    fight.FightResult(hero, monster);
+                case 3:
+                    System.out.println("До встречи!");
+                    game = false;
+                    break;
+            }
+        }
     }
 }
